4e54b2b80932eac20ae12642dde4daf0bc4ed2ad
Trenton Holmes <trenton.holmes@psware.com>
2021-07-01 18:58:22 -0700

    Sketching out a basic idea for the code flow

---
+++ cadence-sensor/cadence-sensor.ino
@@ -0,0 +1,17 @@
+void setup() {
+  // Start serial if wanted
+  // Diplay version
+  // Disable WiFi
+  // Enable BLE
+  // Scan for cadence sensor
+  // Not found, suggest pedaling
+  //    Retry scanning
+  // Found
+  //    Output information
+}
+
+void loop() {
+  // Handle stale data
+  // Display status
+  // Display cadence
+}
