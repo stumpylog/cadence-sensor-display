77a907e4ba74ce12fb1599a42ce4bdbc15d8a68f
Trenton Holmes <trenton.holmes@psware.com>
2021-07-04 11:12:14 -0700

    Sarting to piece together information about the BLE connection

--- cadence-sensor/cadence-sensor.ino
+++ cadence-sensor/cadence-sensor.ino
@@ -3,6 +3,7 @@
 
 // Arduino Libraries
 #include <BLEDevice.h>
+#include <BLEUUID.h>
 
 // Local
 #include "DisplayManager.h"
@@ -10,15 +11,106 @@
 
 // Constants
 // Display - Buttons
-static constexpr uint8_t BUTTON_A{15};
-static constexpr uint8_t BUTTON_B{32};
-static constexpr uint8_t BUTTON_C{14};
+static constexpr uint8_t BUTTON_A{ 15 };
+static constexpr uint8_t BUTTON_B{ 32 };
+static constexpr uint8_t BUTTON_C{ 14 };
+//Bluetooth - speed & cadence UUID
+static BLEUUID const CycleSpeedAndCadenceServiceUUID(static_cast<uint16_t>(0x1816));
+// Bluetooth - notify UUID
+static BLEUUID const NotifyCharacteristicUUID(static_cast<uint16_t>(0x2a5b));
 
 // Program version
 #define VERSION "0.0.1"
 
 // Globals
-static DisplayManager display = DisplayManager();
+
+static boolean doConnect = false;
+static boolean connected = false;
+
+static DisplayManager display;
+static BLERemoteCharacteristic* pRemoteCharacteristic;
+static BLEAdvertisedDevice* myDevice;
+static BLEScan* scanner{ nullptr };
+
+// Called on connect or disconnect
+class ClientCallback : public BLEClientCallbacks {
+  void onConnect(BLEClient* pclient) {
+  }
+  void onDisconnect(BLEClient* pclient) {
+  }
+};
+
+class AdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks {
+  void onResult(BLEAdvertisedDevice advertisedDevice) {
+    Serial.print("BLE Advertised Device found: ");
+    Serial.println(advertisedDevice.toString().c_str());
+
+    // We have found a device, let us now see if it contains the service we are looking for.
+    if (advertisedDevice.haveServiceUUID() && advertisedDevice.isAdvertisingService(CycleSpeedAndCadenceServiceUUID)) {
+
+      BLEDevice::getScan()->stop();
+      myDevice = new BLEAdvertisedDevice(advertisedDevice);
+      doConnect = true;
+    }
+  }  // onResult
+};
+
+static void notifyCallback(BLERemoteCharacteristic* pBLERemoteCharacteristic,
+                           uint8_t* pData,
+                           size_t length,
+                           bool isNotify) {
+  Serial.print("Notify callback for characteristic ");
+  Serial.print(pBLERemoteCharacteristic->getUUID().toString().c_str());
+  Serial.print(" of data length ");
+  Serial.println(length);
+  Serial.print("data: ");
+  Serial.println((char*)pData);
+}
+
+bool connectToServer() {
+  Serial.print("Forming a connection to ");
+  Serial.println(myDevice->getAddress().toString().c_str());
+
+  BLEClient* pClient = BLEDevice::createClient();
+  Serial.println(" - Created client");
+
+  pClient->setClientCallbacks(new ClientCallback());
+
+  // Connect to the remove BLE Server.
+  pClient->connect(myDevice);  // if you pass BLEAdvertisedDevice instead of address, it will be recognized type of peer device address (public or private)
+  Serial.println(" - Connected to server");
+
+  // Obtain a reference to the service we are after in the remote BLE server.
+  BLERemoteService* pRemoteService = pClient->getService(CycleSpeedAndCadenceServiceUUID);
+  if (pRemoteService == nullptr) {
+    Serial.print("Failed to find our service UUID: ");
+    //Serial.println(CycleSpeedAndCadenceServiceUUID.toString().c_str());
+    pClient->disconnect();
+    return false;
+  }
+  Serial.println(" - Found our service");
+
+
+  // Obtain a reference to the characteristic in the service of the remote BLE server.
+  pRemoteCharacteristic = pRemoteService->getCharacteristic(NotifyCharacteristicUUID);
+  if (pRemoteCharacteristic == nullptr) {
+    Serial.print("Failed to find our characteristic UUID: ");
+    //Serial.println(NotifyCharacteristicUUID.toString().c_str());
+    pClient->disconnect();
+    return false;
+  }
+  Serial.println(" - Found our characteristic");
+
+  if (pRemoteCharacteristic->canNotify()) {
+    pRemoteCharacteristic->registerForNotify(notifyCallback);
+  } else {
+    pClient->disconnect();
+    return false;
+  }
+
+  connected = true;
+  return true;
+}
 
 void setup() {
   DebugSerialStart(115200);
@@ -33,15 +125,31 @@ void setup() {
   delay(1000);
 
   // Enable BLE
+  BLEDevice::init("");
   // Scan for cadence sensor
-  // Not found, suggest pedaling
-  //    Retry scanning
-  // Found
-  //    Output information
+  scanner = BLEDevice::getScan();
+  scanner->setAdvertisedDeviceCallbacks(new AdvertisedDeviceCallbacks());
+  scanner->setInterval(1349);
+  scanner->setWindow(449);
+  scanner->setActiveScan(true);
 }
 
 void loop() {
-  // Handle stale data
-  // Display status
-  // Display cadence
+  // If the flag "doConnect" is true then we have scanned for and found the desired
+  // BLE Server with which we wish to connect.  Now we connect to it.  Once we are
+  // connected we set the connected flag to be true.
+  if (true == doConnect) {
+    if (true == connectToServer()) {
+      Serial.println("We are now connected to the BLE Server.");
+    } else {
+      Serial.println("We have failed to connect to the server; there is nothin more we will do.");
+    }
+    doConnect = false;
+  }
+
+  // If we are connected to a peer BLE Server, update the characteristic each time we are reached
+  // with the current time since boot.
+  if (true == connected) {
+    // Display calculated cadence
+  }
 }
